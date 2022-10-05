ffccae62a7c69c6da5f647fde0f06bacd2dab31a
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-10-05 15:46:42 +0900

    POST /member/login 으로 올바르지 않은 username과 password 데이터를 넘기면 400

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
@@ -10,24 +10,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authentication", "JWT키");
 
         String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
 
         return new ResponseEntity<>(body, headers, HttpStatus.OK);
-
-
     }
+
     @Data
     public static class LoginDto {
         private String username;
         private String password;
+
+        public boolean isNotValid() {
+            return username == null || password == null || username.trim().length() == 0 || password.trim().length() == 0;
+        }
     }
 }
