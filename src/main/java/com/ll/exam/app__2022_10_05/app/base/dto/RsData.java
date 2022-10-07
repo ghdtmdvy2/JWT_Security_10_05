fafd487d5c223b22dbcd4e400b725b97ba86d7a3
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-10-07 14:13:11 +0900

    RsData를 따로 처리하여 HTTP 응답에 대한 상태를 분명히 해줌

---
+++ src/main/java/com/ll/exam/app__2022_10_05/app/base/dto/RsData.java
@@ -0,0 +1,30 @@
+package com.ll.exam.app__2022_10_05.app.base.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RsData<T> {
+    // 상태 코드 : F-2 <- 실패의 두번째 것
+    // 상태 코드 : S-3 <- 성공의 세번째 것
+    private String resultCode;
+    // 상태 메세지
+    private String msg;
+    private T data;
+
+    public static <T> RsData<T> of(String resultCode, String msg) {
+        return new RsData<>(resultCode, msg, null);
+    }
+
+    // 성공 했는 지 알려주는 함수.
+    public boolean isSuccess() {
+        return resultCode.startsWith("S-1");
+    }
+
+    public boolean isFail() {
+        return isSuccess() == false;
+    }
+}
--- src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app__2022_10_05.app.member.controller;
 
+import com.ll.exam.app__2022_10_05.app.base.dto.RsData;
 import com.ll.exam.app__2022_10_05.app.member.entity.Member;
 import com.ll.exam.app__2022_10_05.app.member.service.MemberService;
 import lombok.Data;
@@ -24,19 +25,24 @@ public class MemberController {
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<RsData> login(@RequestBody LoginDto loginDto) {
         if (loginDto.isNotValid()) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            // 첫번째 파라미터 : ResponseEntity 의 body 는 화면상 보여주는 내용
+            // 두번째 파라미터 : headers 는 브라우저 내에서 보여주는 내용
+            // 세번째 파라미터 : 브라우저의 상태코드를 보여주는 내용
+
+            // 첫번째 파라미터를 객체로 보내주어, 알아서 자동으로 Json 형태로 뿌려주는 방법을 할 수 있다.
+            return Util.spring.responseEntityOf(RsData.of("F-1", "로그인 정보가 올바르지 않습니다."));
         }
 
         Member member = memberService.findByUsername(loginDto.getUsername()).orElse(null);
 
         if (member == null) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Util.spring.responseEntityOf(RsData.of("F-2", "일치하는 회원이 존재하지 않습니다."));
         }
 
         if (passwordEncoder.matches(loginDto.getPassword(), member.getPassword()) == false) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Util.spring.responseEntityOf(RsData.of("F-3", "비밀번호가 일치하지 않습니다."));
         }
 
         HttpHeaders headers = new HttpHeaders();
@@ -44,7 +50,7 @@ public class MemberController {
 
         String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
 
-        return Util.spring.responseEntityOf(headers);
+        return Util.spring.responseEntityOf(RsData.of("S-1", "로그인 성공, Access Token을 발급합니다."), headers);
     }
 
     @Data
