830662c447f022f73fbdcc9c90cb05e6a9fd0a97
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-10-13 13:41:01 +0900

    캐시 생성

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.ll.exam.app__2022_10_05.app.member.entity.Member;
 import com.ll.exam.app__2022_10_05.app.member.repository.MemberRepository;
 import com.ll.exam.app__2022_10_05.app.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -46,5 +47,10 @@ public class MemberService {
 
     public boolean verifyWithWhiteList(Member member, String token) {
         return member.getAccessToken().equals(token);
+    }
+    @Cacheable("key1")
+    public int getCachedInt() {
+        System.out.println("getCachedInt 호출됨");
+        return 5;
     }
 }
