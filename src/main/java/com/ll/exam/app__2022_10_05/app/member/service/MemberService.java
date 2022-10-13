ac722f8496d8ba5458bf78b5e83e2b253d16c6ef
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-10-13 17:55:27 +0900

    redis 도입 후 사용자 정의 객체( 내가 만든 객체 ) 는 value 값으로 등록이 안되어 map 형태로 수정

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app__2022_10_05.app.member.service;
 
+import com.ll.exam.app__2022_10_05.app.AppConfig;
 import com.ll.exam.app__2022_10_05.app.member.entity.Member;
 import com.ll.exam.app__2022_10_05.app.member.repository.MemberRepository;
 import com.ll.exam.app__2022_10_05.app.security.jwt.JwtProvider;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -48,9 +50,10 @@ public class MemberService {
     public boolean verifyWithWhiteList(Member member, String token) {
         return member.getAccessToken().equals(token);
     }
-
     @Cacheable("member")
-    public Member getByUsername__cached(String username) {
-        return findByUsername(username).orElse(null);
+    public Map<String, Object> getMemberMapByUsername__cached(String username) {
+        Member member = findByUsername(username).orElse(null);
+
+        return member.toMap();
     }
 }
